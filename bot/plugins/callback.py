4c3943de8b6e75e46ccde4ef8c6d5261b858b3d5
grvgk <128710083+grvgk@users.noreply.github.com>
2025-05-24 12:31:25 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -1,24 +1,26 @@
-from telethon.events import CallbackQuery
-from bot import TelegramBot
+from pyrogram import filters
+from pyrogram.types import CallbackQuery
+from bot import app
 from bot.modules.decorators import verify_user
 from bot.modules.static import *
 from bot.modules.telegram import get_message
 
-@TelegramBot.on(CallbackQuery(pattern=r'^rm_'))
+@app.on_callback_query(filters.regex(r'^rm_'))
 @verify_user(private=True)
-async def delete_file(event: CallbackQuery.Event):
-    query_data = event.query.data.decode().split('_')
+async def delete_file(client, callback_query: CallbackQuery):
+    query_data = callback_query.data.split('_')
 
     if len(query_data) != 3:
-        return await event.answer(InvalidQueryText, alert=True)
+        return await callback_query.answer(InvalidQueryText, show_alert=True)
 
     message = await get_message(int(query_data[1]))
 
     if not message:
-        return await event.answer(MessageNotExist, alert=True)
-    if query_data[2] != message.raw_text:
-        return await event.answer(InvalidQueryText, alert=True)
+        return await callback_query.answer(MessageNotExist, show_alert=True)
+
+    if query_data[2] != (message.text or message.caption):
+        return await callback_query.answer(InvalidQueryText, show_alert=True)
 
     await message.delete()
 
-    return await event.answer(LinkRevokedText, alert=True)
+    return await callback_query.answer(LinkRevokedText, show_alert=True)
