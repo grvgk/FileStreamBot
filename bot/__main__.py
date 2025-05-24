9be9ae6d9ba04a89ed4bc97bc8b3ceb52d67eea6
grvgk <128710083+grvgk@users.noreply.github.com>
2025-05-24 13:01:24 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,30 +1,28 @@
 import asyncio
 from importlib import import_module
 from pathlib import Path
-from bot import TelegramBot, logger
-from bot.config import Telegram
+from bot.client import app  # Now imported safely
+from bot import logger
 from bot.server import server
+from pyrogram.idle import idle
 
 def load_plugins():
     count = 0
     for path in Path('bot/plugins').rglob('*.py'):
         import_module(f'bot.plugins.{path.stem}')
         count += 1
-    logger.info(f'Loaded {count} {"plugins" if count != 1 else "plugin"}.')
+    logger.info(f'Loaded {count} {"plugins" if count > 1 else "plugin"}.')
 
 async def main():
-    logger.info("Initializing...")
-    await TelegramBot.start()
-    logger.info("Telegram bot is now started.")
-    
-    # Start the web server
+    logger.info('initializing...')
     asyncio.create_task(server.serve())
-
-    logger.info("Loading bot plugins...")
+    await app.start()
+    logger.info('Telegram client is now started.')
+    logger.info('Loading bot plugins...')
     load_plugins()
-    logger.info("Bot is now ready!")
-    
-    await TelegramBot.idle()
+    logger.info('Bot is now ready!')
+    await idle()
+    await app.stop()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     asyncio.run(main())
