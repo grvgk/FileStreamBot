ad533b4a9f41e4fd903568db34ee1918164ca817
grvgk <128710083+grvgk@users.noreply.github.com>
2025-05-24 12:59:53 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,16 +1,6 @@
-from pyrogram import Client
 from logging import getLogger
 from logging.config import dictConfig
-from .config import Telegram, LOGGER_CONFIG_JSON
+from .config import LOGGER_CONFIG_JSON
 
 dictConfig(LOGGER_CONFIG_JSON)
-
-version = 1.6
 logger = getLogger('bot')
-
-TelegramBot = Client(
-    name="bot",
-    api_id=Telegram.API_ID,
-    api_hash=Telegram.API_HASH,
-    bot_token=Telegram.BOT_TOKEN  # required for bot mode
-)
