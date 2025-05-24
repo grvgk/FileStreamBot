631b75e60b531566f710268c9a261146420c3f85
grvgk <128710083+grvgk@users.noreply.github.com>
2025-05-24 12:33:46 +0530

    Update deeplinks.py

--- bot/plugins/deeplinks.py
+++ bot/plugins/deeplinks.py
@@ -1,24 +1,31 @@
-from telethon.events import NewMessage
-from telethon.tl.custom import Message
-from bot import TelegramBot
+from pyrogram import filters
+from pyrogram.types import Message
+from bot import app
 from bot.modules.decorators import verify_user
 from bot.modules.telegram import get_message, send_message
 from bot.modules.static import *
 
-@TelegramBot.on(NewMessage(incoming=True, pattern=r'^/start file_'))
+@app.on_message(filters.private & filters.command("start"))
 @verify_user(private=True)
-async def send_file(event: NewMessage.Event | Message):
-    payload = event.raw_text.split()[-1].split('_')
+async def send_file(client, message: Message):
+    if not message.text.startswith("/start file_"):
+        return
+
+    payload = message.text.split()[-1].split('_')
 
     if len(payload) != 3:
-        return await event.reply(InvalidPayloadText)
-    
-    message = await get_message(int(payload[1]))
+        return await message.reply(InvalidPayloadText)
 
-    if not message:
-        return await event.reply(MessageNotExist)
-    if payload[2] != message.raw_text:
-        return await event.reply(InvalidPayloadText)
-    
-    message.raw_text = ''
-    await send_message(message, send_to=event.chat_id)
+    original_msg = await get_message(int(payload[1]))
+
+    if not original_msg:
+        return await message.reply(MessageNotExist)
+
+    if payload[2] != (original_msg.text or original_msg.caption):
+        return await message.reply(InvalidPayloadText)
+
+    # Clear text/caption to avoid duplication
+    original_msg.text = ''
+    original_msg.caption = ''
+
+    await send_message(original_msg, send_to=message.chat.id)
