a9e7c41dcc32900b9a6c0a827c78e9c4653c5895
grvgk <128710083+grvgk@users.noreply.github.com>
2025-05-24 12:36:40 +0530

    Update __init__.py

--- bot/server/__init__.py
+++ bot/server/__init__.py
@@ -5,24 +5,29 @@ from bot.config import Server, LOGGER_CONFIG_JSON
 
 from . import main, error
 
-logger = getLogger('uvicorn')
+logger = getLogger("uvicorn")
+
+# Create Quart instance
 instance = Quart(__name__)
 instance.config['RESPONSE_TIMEOUT'] = None
 
 @instance.before_serving
 async def before_serve():
-    logger.info('Web server is started!')
-    logger.info(f'Server running on {Server.BIND_ADDRESS}:{Server.PORT}')
+    logger.info("Web server is starting...")
+    logger.info(f"Server running on {Server.BIND_ADDRESS}:{Server.PORT}")
 
+# Register route blueprints
 instance.register_blueprint(main.bp)
 
+# Register error handlers
 instance.register_error_handler(400, error.invalid_request)
 instance.register_error_handler(404, error.not_found)
 instance.register_error_handler(405, error.invalid_method)
 instance.register_error_handler(error.HTTPError, error.http_error)
 
-server = UvicornServer (
-    Config (
+# Configure Uvicorn to run the Quart app
+server = UvicornServer(
+    Config(
         app=instance,
         host=Server.BIND_ADDRESS,
         port=Server.PORT,
